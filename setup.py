6814bd29e248995e8baa85904bb48aa4b94d48c3
Kevin Chang <kckaiwei@gmail.com>
2016-06-23 10:48:49 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='GameDealBot',
-      version='1.0.0',
+      version='1.1.0',
       description='reddit bot for scraping Game Deals',
 	  author = 'Kevin C.',
 	  author_email = 'kckaiwei@gmail.com',
